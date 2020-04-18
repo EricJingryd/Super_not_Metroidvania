3d4f6e4697104aa62152215e4656cd038e917a85
JulioDaher <62564438+JulioDaher@users.noreply.github.com>
2020-04-18 16:41:20 +0200

    Grab mechanics
    Scripts, prefabs, etc

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Destructable"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Grabber.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour
+{
+
+    public bool grabbed;
+    RaycastHit2D hit;
+    public float distance = 2f;
+    public float throwforce;
+    public Transform Holdpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!grabbed)
+            {
+                hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance); //Uses raycast in front of player to check for collision
+
+                if (hit.collider != null && hit.collider.gameObject.CompareTag("Bomb"))
+                {
+                    grabbed = true;
+                }
+            }
+
+            else
+            {
+                grabbed = false;
+
+                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null) //Throws object
+                {
+                    hit.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+                }
+            }
+        }
+
+        if (grabbed) //Keeps the object at a point that is always above player
+        {
+            hit.collider.gameObject.transform.position = Holdpoint.position;
+        }
+    }
+
+    private void OnDrawGizmos() //Visual aid
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.right * transform.localScale.x*distance);
+    }
+}
