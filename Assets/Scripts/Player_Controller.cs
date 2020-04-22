3dd10642fca1d9776be7c3e1523c295ab5d32901
RobinHuynh <robin.huynh@hotmail.com>
2020-04-22 22:08:19 +0200

    Fixat player movement
    Annat script för player movement.
    Bytt player sprites till Warped Gates.
    Animationer för:
    Jumping
    Crouching
    Running

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40;
+
+    float horizontalMovement = 0f;
+
+    bool jump = false;
+    bool crouch = false;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouching (bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
