669ce093fdea2403a7603e17d1d63f7d5bcc9f5c
JulioDaher <62564438+JulioDaher@users.noreply.github.com>
2020-04-27 10:38:36 +0200

    Bomb related
    New brush with destructable blocks.
    Bomb now hits in an AoE
    Blocks get respawned when you leave scene, needs fixing

---
+++ Assets/Scripts/Destroyblock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyblock : MonoBehaviour
+{
+    //static Destroyblock instance;
+
+    //private void Start()
+    //{
+    //    if(instance != null)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //    else
+    //    {
+    //        instance = this;
+    //        DontDestroyOnLoad(gameObject);
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bomb"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Items/Bomb.cs
+++ Assets/Scripts/Items/Bomb.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    public GameObject destroyEffect;
+    public float bombRadius;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Destructable"))
         {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
+            Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            Collider2D[] objectsToDamage = Physics2D.OverlapCircleAll(transform.position, bombRadius);
+            for (int i = 0; i < objectsToDamage.Length; i++)
+            {
+                if (objectsToDamage[i].gameObject.CompareTag("Destructable"))
+                {
+                    Destroy(objectsToDamage[i].gameObject);
+                }
+            }
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, bombRadius);
     }
 }
